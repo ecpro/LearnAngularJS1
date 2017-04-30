34d0e481d8bb2ff69504afcfa349cfc560f1d35c
Piyush Ravi <mails4ipiyush@gmail.com>
2017-04-30 18:22:09 +0530

    second directive with object mapping from controller scope to local scope

--- app.js
+++ app.js
@@ -20,20 +20,26 @@ app.config(function ($routeProvider) {
 app.controller('mainController', ['$scope', '$log', 'nameService', function ($scope, $log, nameService) {
     $scope.pageName = 'Main';
     $scope.sometext = nameService.name;
-    
-    $scope.$watch("sometext", function() {
+
+    $scope.$watch("sometext", function () {
         nameService.name = $scope.sometext;
     });
-    
+
 }]);
 
 app.controller('secondController', ['$scope', '$log', 'nameService', function ($scope, $log, nameService) {
     $scope.pageName = "second";
     $scope.sometext = nameService.name;
-    
-    $scope.$watch("sometext", function() {
+
+    $scope.$watch("sometext", function () {
         nameService.name = $scope.sometext;
     });
+
+    $scope.person = {
+        firstName: "Oliver",
+        lastName: "Queen",
+        age: 14
+    };
 
 
 }]);
@@ -44,40 +50,30 @@ app.service("nameService", function () {
 
 });
 
-app.directive('myDirective', function() {
-    
+
+app.directive('myDirective', function () {
+
     return {
-        templateUrl :  'directives/directive.html',
-        restrict : 'AECM',  // A - attribute, E - element, C - class, M - comment
-        replace : true,
-        scope  : {
-            pageName : "@pageName",
-            sometext : "@sometext"
-    }
+        templateUrl: 'directives/directive.html',
+        restrict: 'AECM', // A - attribute, E - element, C - class, M - comment
+        replace: true,
+        scope: {
+            pageName: "@pageName", // making $scope.pageName in controller accessible (one way binding of string) 
+            sometext: "@" // or "@sometext" one way binding shorthand
+        }
     };
 });
 
+// if scope key is not defined at all then it inherits the parent scope ie, the scope of controller that controller this directive
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.directive("youDirective", function () {
+    return {
+        templateUrl: "directives/secondDirective.html",
+        restrict : 'AE',
+        replace : true,
+        scope : {
+            personObject : "=" // or "=person" making $scope.person accesible here. Also this is two way binding. If anything changes personObject then person in secondController scope will also change. So be carefull about it.
+    }
+    },
+})
